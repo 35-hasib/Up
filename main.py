0208d93cc937f77db564d9733a9e4eb4c9649245
35-hasib <hrrahman35@gmail.com>
2024-08-02 14:37:49 +0600

    add all

--- main.py
+++ main.py
@@ -20,6 +20,15 @@ while True:
         driver.maximize_window()
         driver.delete_all_cookies()
         driver.get('https://codeforces.com/enter?back=%2F')
+        
+        time.sleep(2)
+        driver.find_element(By.NAME, "handleOrEmail").send_keys(name)
+        driver.find_element(By.NAME, "password").send_keys(pas)
+        driver.find_element(by='xpath', value='//tbody/tr[4]/td[1]/div[1]/input[1]').click()
+        print('login successfull....!')
+        time.sleep(2)
+        
+        
     
         driver.close();
         i = i+1
